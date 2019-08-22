662aee7b9151eff385c1c77c6cd98ebba28c0ee2
Jonas <jgalmeida1993@gmail.com>
2019-08-22 10:31:33 -0300

    :tada: Primeiro commit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyA9S5qDTxbBFzguVpLVnXlzchnrYsyde6U",
+  authDomain: "entendaantes.firebaseapp.com",
+  databaseURL: "https://entendaantes.firebaseio.com",
+  projectId: "entendaantes",
+  storageBucket: "entendaantes.appspot.com",
+  messagingSenderId: "346326807274",
+  appId: "1:346326807274:web:2868a7f272cc8ec4"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
